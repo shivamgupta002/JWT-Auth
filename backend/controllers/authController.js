1cdcd66b9c72a9fa4cca4b2f9d6bd60d22e2791e
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-08 16:48:02 +0530

    Registration part completed with password hashing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,44 @@
+import authModel from "../models/authModel.js";
+import bcryptjs from "bcryptjs";
+
 class authController {
   static userRegistration = async (req, res) => {
-    res.send("welcome to register");
+    const { name, email, password } = req.body;
+    try {
+      if (name && email && password) {
+        const isUser = await authModel.findOne({ email: email });
+        if (isUser) {
+          return res.status(400).json({ message: "User already found" });
+        } else {
+          //password hashing
+          const genSalt = await bcryptjs.genSalt(10);
+          const hashedPassword = await bcryptjs.hash(password, genSalt);
+
+          //save the User
+          const newUser = authModel({
+            name,
+            email,
+            password: hashedPassword,
+          });
+          const reUser = await newUser.save();
+          if (reUser) {
+            return res
+              .status(200)
+              .json({ message: "Successfully Register", user: reUser });
+          }
+        }
+      } else {
+        return res.status(400).json({ message: "All Fields are required" });
+      }
+    } catch (e) {
+      return res.status(400).json({ message: e.message });
+    }
   };
 }
 export default authController;
+
+//-------------------------Also done by function -------------------------
+// const authController = (req, res) => {
+//   res.send("welcome");
+// };
+// export default authController;
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import connectDB from "./config/db.js";
 const app = express();
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 //--------------  MongoDB Connection Setup  ---------------
 connectDB();
 
+app.use(cors());
+app.use(express.json());
 //--------------  Routes ---------------
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
