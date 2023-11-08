ecf890a981a20ba007ca23476e0c6a3dc65585d2
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-08 18:14:50 +0530

    Login backed complete

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,9 @@
 import authModel from "../models/authModel.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 class authController {
+  // ----------------------- SignUp -----------------------------------
   static userRegistration = async (req, res) => {
     const { name, email, password } = req.body;
     try {
@@ -29,6 +31,36 @@ class authController {
         }
       } else {
         return res.status(400).json({ message: "All Fields are required" });
+      }
+    } catch (e) {
+      return res.status(400).json({ message: e.message });
+    }
+  };
+
+  // ----------------------- Login -----------------------------------
+  static userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      if (email && password) {
+        const isUser = await authModel.findOne({ email: email });
+        if (isUser) {
+          if (
+            email === isUser.email &&
+            (await bcryptjs.compare(password, isUser.password))
+          ) {
+            // Generate Token
+            const token = jwt.sign({ userId: isUser._id }, "shivam", {
+              expiresIn: "2d",
+            });
+            return res.status(200).json({ message: "Login Successful", token });
+          } else {
+            return res.status(400).json({ message: "Invalid Credentials" });
+          }
+        } else {
+          return res.status(400).json({ message: "User not Registered" });
+        }
+      } else {
+        return res.status(400).json({ message: "All fields are required" });
       }
     } catch (e) {
       return res.status(400).json({ message: e.message });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import authController from "../controllers/authController.js";
+import authController from "../controllers/authCOntroller.js";
 const router = express.Router();
 
 router.post("/users/register", authController.userRegistration);
+router.post("/users/login", authController.userLogin);
 
 export default router;
