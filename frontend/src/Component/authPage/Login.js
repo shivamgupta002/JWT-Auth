6ba17841c8ff72fce4645e374d386fc4c1caced0
shivamgupta002 <shivamgupta992909@gmail.com>
2023-11-09 16:43:27 +0530

    Final touch

--- frontend/src/Component/authPage/Login.js
+++ frontend/src/Component/authPage/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "../Axios/Api";
+import axios from "../Services/Api";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
